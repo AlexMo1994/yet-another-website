e1f8e1b358c8a9ca9ad2f5e3f494b85314ac4f06
Alex <fwwbd@yandex.ru>
2022-08-21 22:49:49 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//вызов данной функции скрывает или показывает элемент
+const showMenu = function (className) {
+    const el = document.getElementById(className);
+    el.classList.toggle("show-element");
+}
+
+// document.get
+
